18cd0af526c2e14a81bf5c8ffcd796495cd228e6
hojung <hojungjeong123@hotmail.co.kr>
2014-12-09 22:31:39 +0900

    Silver Bullet Encryption Tool

--- Silver Bullet Encryption Tool/bullet_parts/primer.py
+++ Silver Bullet Encryption Tool/bullet_parts/primer.py
@@ -15,7 +15,7 @@ def encrypt(user_input):
 
 
     for element in raw_list:
-        key_value=propellant.generate()
+        key_value=propellant.propellant()
 
         key_gen=key_value%end
         encrypted=element+key_gen
