30d410504be4c9c4f461e94318a0cb293df059db
hojung <hojungjeong123@hotmail.co.kr>
2015-01-03 23:59:21 +0000

    Silver Bullet Encryption Tool

--- Silver Bullet Encryption Tool/bullet_parts/propellant.py
+++ Silver Bullet Encryption Tool/bullet_parts/propellant.py
@@ -8,96 +8,157 @@ def propellant():
 
 
 		def zero():
+			start=0
+
+			while start<=0:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('0')
+				opener.write(str(start))
+
 
 		def one():
+			start=0
+
+			while start<=1:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('1')
+				opener.write(str(start))
+
 
 		def two():
+			start=0
+
+			while start<=2:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('2')
+				opener.write(str(start))
+
 
 		def three():
+			start=0
+
+			while start<=3:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('3')
+				opener.write(str(start))
+
 
 		def four():
+			start=0
+
+			while start<=4:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('4')
+				opener.write(str(start))
 
 		def five():
+			start=0
+
+			while start<=5:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('5')
+				opener.write(str(start))
 
 		def six():
+			start=0
+
+			while start<=6:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('6')
+				opener.write(str(start))
 
 		def seven():
+			start=0
+
+			while start<=7:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('7')
+				opener.write(str(start))
 
 		def eight():
+			start=0
+
+			while start<=8:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('8')
+				opener.write(str(start))
 
 		def nine():
+			start=0
+
+			while start<=9:
+				start+=1
+
 			with open (num_gen, 'a') as opener:
-				opener.write('9')
+				opener.write(str(start))
 
 
+		#==================================================
 
+		def first():
+			thread_1=Thread(target=zero)
+			thread_2=Thread(target=nine)
+			thread_1.start()
+			thread_2.start()
 
-		def thread_set_1():
-			thread_0=Thread(target=zero)
+		def second():
 			thread_1=Thread(target=one)
-			thread_2=Thread(target=two)
-			thread_3=Thread(target=three)
-			thread_4=Thread(target=four)
-			thread_5=Thread(target=five)
-			thread_6=Thread(target=six)
-			thread_7=Thread(target=seven)
-			thread_8=Thread(target=eight)
-			thread_9=Thread(target=nine)
-			thread_0.start()
+			thread_2=Thread(target=eight)
 			thread_1.start()
 			thread_2.start()
-			thread_3.start()
-			thread_4.start()
-			thread_5.start()
-			thread_6.start()
-			thread_7.start()
-			thread_8.start()
-			thread_9.start()
 
-		def thread_set_2():
-			thread_0=Thread(target=nine)
-			thread_1=Thread(target=eight)
+		def third():
+			thread_1=Thread(target=two)
 			thread_2=Thread(target=seven)
-			thread_3=Thread(target=six)
-			thread_4=Thread(target=five)
-			thread_5=Thread(target=four)
-			thread_6=Thread(target=three)
-			thread_7=Thread(target=two)
-			thread_8=Thread(target=one)
-			thread_9=Thread(target=zero)
-			thread_0.start()
+			thread_1.start()
+			thread_2.start()
+
+		def fourth():
+			thread_1=Thread(target=three)
+			thread_2=Thread(target=six)
+			thread_1.start()
+			thread_2.start()
+
+		def fifth():
+			thread_1=Thread(target=four)
+			thread_2=Thread(target=five)
+			thread_1.start()
+			thread_2.start()
+
+		#==================================================
+
+		def thread_set():
+			thread_1=Thread(target=first)
+			thread_2=Thread(target=second)
+			thread_3=Thread(target=third)
+			thread_4=Thread(target=fourth)
+			thread_5=Thread(target=fifth)
 			thread_1.start()
 			thread_2.start()
 			thread_3.start()
 			thread_4.start()
 			thread_5.start()
-			thread_6.start()
-			thread_7.start()
-			thread_8.start()
-			thread_9.start()
 
-		thread_1=Thread(target=thread_set_1)
-		thread_2=Thread(target=thread_set_2)
-		thread_1.start()
-		thread_2.start()
+
+		def final_set():
+			thread=Thread(target=thread_set)
+			thread.start()
+
+
+		ithread_1=Thread(target=final_set)
+		ithread_2=Thread(target=final_set)
+		ithread_1.start()
+		ithread_2.start()
+
 
 
 		reader=open(num_gen, 'r').read()
