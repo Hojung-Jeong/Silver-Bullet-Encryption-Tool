bfbab78e8a53e213493e508a4cdd3b2ee98c377f
hojung <hojungjeong123@hotmail.co.kr>
2015-01-30 20:17:35 +0000

    Silver Bullet Encryption Tool

--- Silver Bullet Encryption Tool/bullet_parts/primer.py
+++ Silver Bullet Encryption Tool/bullet_parts/primer.py
@@ -3,8 +3,8 @@ import random
 
 
 
-the_number=256
-
+end=256
+ascii_buffer=256
 
 
 def encrypt(user_input, passphrase):
@@ -24,13 +24,13 @@ def encrypt(user_input, passphrase):
     for element in raw_list:
         key_value=propellant.propellant()
 
-        key_gen=key_value%the_number
+        key_gen=key_value%end
         encrypted=element+key_gen
-        acceptable=encrypted%the_number
+        acceptable=encrypted%end
 
-        key.append(chr(key_gen+the_number-random.randrange(the_number)))
+        key.append(chr(key_gen+ascii_buffer-random.randrange(ascii_buffer)))
 
-        filtred.append(chr(acceptable+the_number))
+        filtred.append(chr(acceptable+ascii_buffer))
 
 
     finished_string=''.join(filtred)
@@ -67,21 +67,21 @@ def decrypt (encrypted_string, key, passphrase):
 
 
     for element in encrypted_list:
-        string.append(ord(element)-the_number)
+        string.append(ord(element)-ascii_buffer)
 
     for element in key_list:
-        key.append(ord(element)-the_number)
+        key.append(ord(element)-ascii_buffer+random.randrange(ascii_buffer))
 
 
     decrypted_list=[]
 
     for index in range(len(string)):
-        decrypted_element=string[index]-key[index]+random.randrange(the_number)
+        decrypted_element=string[index]-key[index]
 
         if decrypted_element>=0:
             decrypted_list.append(decrypted_element)
         else:
-            final=decrypted_element+the_number
+            final=decrypted_element+end
             decrypted_list.append(final)
 
     byted=bytes(decrypted_list)
