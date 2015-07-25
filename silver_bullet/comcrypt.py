3b1d723dff3188a60811f3942f2edf6c93a9b17a
Hojung <hojungjeong123@hotmail.co.kr>
2015-07-25 17:20:25 +0900

    code review 25.07.15

--- silver_bullet/comcrypt.py
+++ silver_bullet/comcrypt.py
@@ -17,17 +17,17 @@ import sys
 from silver_bullet.symcrypt import sym_encrypt_core, sym_decrypt_core
 
 
-def scom_encrypt(user_input, passphrase):
-	compressed = zlib.compress(user_input.encode())
-	ui_listed = list(compressed)
-	cipher_text, locked_pad = sym_encrypt_core(ui_listed, passphrase)
+def scom_encrypt(user_input,passphrase):
+	compressed= lib.compress(user_input.encode())
+	ui_listed=list(compressed)
+	cipher_text,locked_pad=sym_encrypt_core(ui_listed,passphrase)
 
-	return (cipher_text, locked_pad)
+	return (cipher_text,locked_pad)
 
 
-def scom_decrypt(cipher_text, locked_pad, passphrase):
-	deciphered = sym_decrypt_core(cipher_text, locked_pad, passphrase)
-	byted = bytes(deciphered)
-	decompressed = zlib.decompress(byted).decode()
+def scom_decrypt(cipher_text,locked_pad,passphrase):
+	deciphered=sym_decrypt_core(cipher_text,locked_pad,passphrase)
+	byted=bytes(deciphered)
+	decompressed=zlib.decompress(byted).decode()
 
 	return decompressed
--- silver_bullet/symcrypt.py
+++ silver_bullet/symcrypt.py
@@ -20,20 +20,20 @@ import random
 from silver_bullet.TRNG import trng
 
 
-ascii_value = 256
+ascii_value=256
 
 def contain_ascii(value):
-	if value < 0:
-		return value + ascii_value
+	if value<0:
+		return value+ascii_value
 
-	elif value >= ascii_value:
-		return value - ascii_value
+	elif value>=ascii_value:
+		return value-ascii_value
 	else:
 		return value
 
 
 def decide_type(value):
-	int_or_str = trng() % 2
+	int_or_str=trng()%2
 
 	if int_or_str is 0:
 		return int(value)
@@ -46,83 +46,83 @@ def pad_gen(ui_listed):
 	pad=[0 for num in range(len(ui_listed))]
 
 	for counter in range(20):
-		op_decider = trng() % 3
-		actual_seed = trng()
+		op_decider=trng()%3
+		actual_seed=trng()
 
 		if op_decider is 0:
 			random.seed(decide_type(actual_seed))
-			pad=[contain_ascii(element + random.randrange(ascii_value)) for element in pad]
+			pad=[contain_ascii(element+random.randrange(ascii_value)) for element in pad]
 		elif op_decider is 1:
 			random.seed(decide_type(actual_seed))
-			pad=[contain_ascii(element - random.randrange(ascii_value)) for element in pad]
+			pad=[contain_ascii(element-random.randrange(ascii_value)) for element in pad]
 		elif op_decider is 2:
 			random.seed(decide_type(actual_seed))
-			pad=[element ^ random.randrange(ascii_value) for element in pad]		
+			pad=[element^random.randrange(ascii_value) for element in pad]		
 
 	return pad
 
 #------------------------------------------------------------------
 
-def sym_encrypt_core(ui_encoded, passphrase):
-	ui_listed = list(ui_encoded)
-	pad = pad_gen(ui_listed)
+def sym_encrypt_core(ui_encoded,passphrase):
+	ui_listed=list(ui_encoded)
+	pad=pad_gen(ui_listed)
 	
-	ct = []
-	counter = 0
+	ct=[]
+	counter=0
 
 	for element in ui_listed:
-		ciphered = contain_ascii(element + pad[counter])
+		ciphered=contain_ascii(element+pad[counter])
 		ct.append(ciphered)
-		counter += 1
+		counter+=1
 
 	random.seed(passphrase)
-	lp = []
-	counter = 0
+	lp=[]
+	counter=0
 
 	for element in pad:
-		locked = element ^ random.randrange(ascii_value)
+		locked=element^random.randrange(ascii_value)
 		lp.append(locked)
-		counter += 1
+		counter+=1
 
-	cipher_text = ' '.join(map(str, ct))
-	locked_pad = ' '.join(map(str, lp))
+	cipher_text=' '.join(map(str, ct))
+	locked_pad=' '.join(map(str, lp))
 	return cipher_text, locked_pad
 
 
-def sym_decrypt_core(cipher_text, locked_pad, passphrase):
-	ct = cipher_text.split(' ')
-	lp = locked_pad.split(' ')
+def sym_decrypt_core(cipher_text,locked_pad,passphrase):
+	ct=cipher_text.split(' ')
+	lp=locked_pad.split(' ')
 
 	random.seed(passphrase)
-	pad = []
-	counter = 0
+	pad=[]
+	counter=0
 
 	for element in lp:
-		unlocked = int(element) ^ random.randrange(ascii_value)
+		unlocked=int(element)^random.randrange(ascii_value)
 		pad.append(unlocked)
-		counter += 1
+		counter+=1
 
-	pt = []
-	counter = 0
+	pt=[]
+	counter=0
 
 	for element in ct:
-		deciphered = contain_ascii(int(element) - pad[counter])
+		deciphered=contain_ascii(int(element)-pad[counter])
 		pt.append(deciphered)
-		counter += 1
+		counter+=1
 
 	return bytes(pt)
 
 #------------------------------------------------------------------
 
-def sym_encrypt(user_input, passphrase):
-	encoded = user_input.encode()
-	cipher_text, locked_pad = sym_encrypt_core(encoded, passphrase)
+def sym_encrypt(user_input,passphrase):
+	encoded=user_input.encode()
+	cipher_text,locked_pad=sym_encrypt_core(encoded, passphrase)
 
-	return cipher_text, locked_pad
+	return cipher_text,locked_pad
 
 
-def sym_decrypt(cipher_text, locked_pad, passphrase):
-	byted_pt = sym_decrypt_core(cipher_text, locked_pad, passphrase)
-	plain_text = byted_pt.decode()
+def sym_decrypt(cipher_text,locked_pad,passphrase):
+	byted_pt=sym_decrypt_core(cipher_text,locked_pad,passphrase)
+	plain_text=byted_pt.decode()
 
 	return plain_text
