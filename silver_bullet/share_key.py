3b34ab590d0b188ed221eaeb290a974f78d63542
Hojung <hojungjeong123@yahoo.com>
2015-08-21 10:05:18 +0900

    Finally, uncrackable key exchange has come!

--- silver_bullet/share_key.py
+++ silver_bullet/share_key.py
@@ -7,6 +7,74 @@
 
 # CODE ========================================================================
 
+import random
+from hashlib import sha1
+from silver_bullet.TRNG import trlist
+from silver_bullet.crypto import encrypt,decrypt
+
+
+numofdigit=9
+rounds=20
+test_text="Tadah!!!"
+
+
+def gen_key(raw_value):
+	raw_length=len(raw_value)
+	first=raw_value[:round(raw_length/3)]
+	second=raw_value[round(raw_length/3):round(raw_length/3)*2]
+	third=raw_value[round(raw_length/3)*2:round(raw_length/3)*3]
+
+	key=sha1(str(int(first,2)).encode()).hexdigest()
+	key=key+sha1(str(int(second,2)).encode()).hexdigest()
+	sliced_key=list(key)
+	seed_string=sha1(str(int(third,2)).encode()).hexdigest()
+	random.seed(seed_string)
+	random.shuffle(sliced_key)
+	key=''.join(sliced_key)
+
+	return key
+
+
+def letsbegin():
+	secret_list=trlist(numofdigit*rounds,2)
+	
+	secret=''.join(map(str,secret_list))
+	secret_key=gen_key(secret)
+	cipher_text,locked_pad=encrypt(test_text,secret_key)
+
+	return secret,secret_key,cipher_text,locked_pad
+
+
+def shuffleit(toshuffle,passphrase):
+	shuffled=[]
+
+	for counter in range(rounds):
+		start=counter*numofdigit
+		target=toshuffle[start:start+9]
+		tlist=list(target)
+		random.seed(passphrase)
+		random.shuffle(tlist)
+		shuffled.append(''.join(tlist))
+
+	return ''.join(shuffled)
+
+
+def ppcheck(exchanged,cipher_text,locked_pad):
+	secret_key=gen_key(exchanged)
+
+	try:
+		plain_text=decrypt(cipher_text,locked_pad,secret_key)
+
+		if plain_text==test_text:
+			return True
+		else:
+			return False
+
+	except:
+		return False
+
+
+
 
 
 '''
